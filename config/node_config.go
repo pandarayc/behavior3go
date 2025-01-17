b063c9a972863d9e82bf1b0dbb369e6978c0827e
genet <raydraw@163.com>
2025-01-17 03:31:17 +0000

    feat: config 读取配置

--- config/node_config.go
+++ config/node_config.go
@@ -1,6 +1,9 @@
 package config
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // 节点配置
 type NodeCfg struct {
@@ -16,4 +19,66 @@ type NodeCfg struct {
 func (t *NodeCfg) String() string {
 	str, _ := json.Marshal(t)
 	return string(str)
+}
+
+func (t *NodeCfg) getProperties(name string) interface{} {
+	v, ok := t.Properties[name]
+	if !ok {
+		panic(fmt.Sprintf("nodeCfg:%+v no prop:%+v", t, name))
+	}
+	return v
+}
+
+func (t *NodeCfg) GetPropertiesAsInt32(name string) int32 {
+	v := t.getProperties(name)
+	val, ok := v.(int32)
+	if !ok {
+		panic(fmt.Sprintf("nodeId:%+v prop:%v fail", t.Name, name))
+	}
+	return val
+}
+
+func (t *NodeCfg) GetPropertiesAsInt64(name string) int64 {
+	v := t.getProperties(name)
+	val, ok := v.(int64)
+	if !ok {
+		panic(fmt.Sprintf("nodeId:%+v prop:%v fail", t.Name, name))
+	}
+	return val
+}
+
+func (t *NodeCfg) GetPropertiesAsFloat32(name string) float32 {
+	v := t.getProperties(name)
+	val, ok := v.(float32)
+	if !ok {
+		panic(fmt.Sprintf("nodeId:%+v prop:%v fail", t.Name, name))
+	}
+	return val
+}
+
+func (t *NodeCfg) GetPropertiesAsFloat64(name string) float64 {
+	v := t.getProperties(name)
+	val, ok := v.(float64)
+	if !ok {
+		panic(fmt.Sprintf("nodeId:%+v prop:%v fail", t.Name, name))
+	}
+	return val
+}
+
+func (t *NodeCfg) GetPropertiesAsBool(name string) bool {
+	v := t.getProperties(name)
+	val, ok := v.(bool)
+	if !ok {
+		panic(fmt.Sprintf("nodeId:%+v prop:%v fail", t.Name, name))
+	}
+	return val
+}
+
+func (t *NodeCfg) GetPropertiesAsString(name string) string {
+	v := t.getProperties(name)
+	val, ok := v.(string)
+	if !ok {
+		panic(fmt.Sprintf("nodeId:%+v prop:%v fail", t.Name, name))
+	}
+	return val
 }
